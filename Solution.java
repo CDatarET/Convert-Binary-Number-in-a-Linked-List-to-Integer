d56f5d3ae7a5816b99e577984d90af09558ab1fd
Chaitanya Datar <datar.chaitanya@gmail.com>
2025-11-30 23:19:22 +0530

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int getDecimalValue(ListNode head) {
+        String s = "";
+        ListNode current = head;
+        while(current != null){
+            s = s + current.val;
+            current = current.next;
+        }
+
+        return Integer.parseInt(s, 2);
+    }
+}
